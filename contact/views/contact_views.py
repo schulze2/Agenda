07f2b948adc02dc7d199e530246b24921ccc2ff7
schulze2 <germano_schulze_junior@hotmail.com>
2023-10-05 09:19:40 -0300

    Criando arquivos parciais com include nos templates

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,7 +8,8 @@ def index(request):
         .order_by('-id')[:10]
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -23,8 +24,11 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact.objects.filter(pk=contact_id), show=True)
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
